05b09757e891f61d6d8d3ffb4e59a060d632569c
bodhichristian <clavelle23@gmail.com>
2024-06-17 14:27:40 -0400

    Create project file

---
+++ ToDoozy/ToDoozy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ToDoozy
+//
+//  Created by christian on 6/17/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ToDoozy/ToDoozyTests/ToDoozyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoozyTests.swift
+//  ToDoozyTests
+//
+//  Created by christian on 6/17/24.
+//
+
+import Testing
+@testable import ToDoozy
+
+struct ToDoozyTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
